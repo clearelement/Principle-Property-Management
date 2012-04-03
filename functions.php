3896cccbc63edcae55cab9b1fefc32aecc9eed62
Dave Crow <dave@clearelement.info>
2012-04-03 00:31:56 -0500

    updated URLs for production, adjusted .intro-copy margin and .polaroid border/margin for iPhone

--- functions.php
+++ functions.php
@@ -18,14 +18,14 @@ function ppm_fluid_header() {
     	
     	<div class="site-logo group">
 	    	<h1>
-	    		<a href="http://www.principleproperty.dev/">
+	    		<a href="http://www.principleproperty.net/">
 	    			<img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo.png" alt="Principle Property Management" title="Return to the Principle Property Management homepage">
 	    		</a>
 	    	</h1>
 	    	<p class="vcard"><span class="tel value">309.686.1122</span></p>
     	</div>
-    	<nav class="primary prairie-vista"> <a href="/prairievista">Prairie Vista Apartments</a> </nav>
-	    <nav class="primary woodsage"> <a href="/woodsage">Woodsage Apartments</a> </nav>
+    	<nav class="primary prairie-vista"> <a href="/prairie-vista-apartments-peoria-il">Prairie Vista Apartments</a> </nav>
+	    <nav class="primary woodsage"> <a href="/woodsage-apartments-peoria-il">Woodsage Apartments</a> </nav>
     </div>
     </header>   
 <?php 
@@ -41,8 +41,8 @@ function ppm_fluid_footer() {
 	<footer class="site-footer group">
 	<div class="fluid-wrapper group">
 		<div class="footer-apartments">
-			<a href="/prairievista"> <img src="<?php bloginfo('stylesheet_directory'); ?>/img/prairie-vista-footer.png" alt="Prairie Vista Luxury Apartments" title="Prairie Vista Luxury Apartments"> </a>
-			<a href="/woodsage"> <img src="<?php bloginfo('stylesheet_directory'); ?>/img/woodsage-footer.png" alt="Woodsage Apartments" title="Woodsage Apartments"> </a>
+			<a href="/prairie-vista-apartments-peoria-il/"> <img src="<?php bloginfo('stylesheet_directory'); ?>/img/prairie-vista-footer.png" alt="Prairie Vista Luxury Apartments" title="Prairie Vista Luxury Apartments"> </a>
+			<a href="/woodsage-apartments-peoria-il"> <img src="<?php bloginfo('stylesheet_directory'); ?>/img/woodsage-footer.png" alt="Woodsage Apartments" title="Woodsage Apartments"> </a>
 		</div>
 		<div class="footer-rentals">
 			<a href="http://principle.managebuilding.com/">See All of Our <b>Available Rentals</b></a>
